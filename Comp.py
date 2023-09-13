2d38dcb2eca99e1134b47c2831bee6d4515149f6
John Doe <jack.h.sawyer@pm.me>
2023-09-13 06:30:49 -0700

    Added date and time

--- Comp.py
+++ Comp.py
@@ -1,7 +1,9 @@
-x = int(input())
-y = int(input())
-z = int(input())
-n = int(input())
+from datetime import datetime
+
+x = int(input('First Parameter: '))
+y = int(input('Second Parameter: '))
+z = int(input('Third Parameter: '))
+n = int(input('Fourth Parameter: '))
 
 coord = [[i, j, k] 
          for i in range(x + 1) 
@@ -9,4 +11,9 @@ coord = [[i, j, k]
          for k in range(z + 1) 
          if i + j + k != n]
 
+now = datetime.now()
+
 print(coord)
+
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
